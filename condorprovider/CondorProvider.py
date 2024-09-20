5e1ecdafc649e611c95106be0dbeb100178340ae
Lucio Anderlini <Lucio.Anderlini@cern.ch>
2024-09-20 08:52:14 +0200

    fix: very creatively, interlink LogRequest uses PascalCase fields

--- condorprovider/CondorProvider.py
+++ condorprovider/CondorProvider.py
@@ -154,7 +154,7 @@ class CondorProvider(interlink.provider.Provider):
 
         with tarfile.open(fileobj=output_struct['logs'], mode='r:*') as tar:
             for member in tar.getmembers():
-                if member.isfile() and member.name == log_request.containerName:
+                if member.isfile() and member.name == log_request.ContainerName:
                     full_log = tar.extractfile(member).read().decode('utf-8')
 
         if log_request.Opts.Tail is not None:
