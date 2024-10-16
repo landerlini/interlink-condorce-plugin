8334b52fde8b7ddbe3768e70758dcacfbff004f1
Lucio Anderlini <Lucio.Anderlini@cern.ch>
2024-10-16 18:36:30 +0200

    Fixed missing comma

--- condorprovider/apptainer_cmd_builder/containers.py
+++ condorprovider/apptainer_cmd_builder/containers.py
@@ -285,7 +285,7 @@ class ContainerSpec(BaseModel, extra="forbid"):
                 f"if [ -f {local_image} ]; then",
                 f"  IMAGE_{uid}={local_image}",
                 f"else",
-                f"  HTTP_STATUS=$(curl -Lo {os.path.join(self.scratch_area, f'.img.{uid}')} \\"
+                f"  HTTP_STATUS=$(curl -Lo {os.path.join(self.scratch_area, f'.img.{uid}')} \\",
                 f"      -w \"%{{http_code}}\" \\",
                 f"      -H \"X-Token: {self.shub_token}\" \\",
                 f"      {SHUB_PROXY}/get-docker/{self.image}) ",
