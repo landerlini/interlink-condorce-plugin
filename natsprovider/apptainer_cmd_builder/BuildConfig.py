958ab5ddcc1caed81ecb8ab56179b5b1fcb85415
Lucio Anderlini <Lucio.Anderlini@cern.ch>
2025-10-10 12:42:22 +0200

    Using cvmfs unpacked from INFN as per default

--- natsprovider/apptainer_cmd_builder/BuildConfig.py
+++ natsprovider/apptainer_cmd_builder/BuildConfig.py
@@ -31,7 +31,7 @@ class BuildConfig(BaseModel):
             description="Location where to look for pre-built images",
         )
         cvmfs_unpacked_path: Optional[str] = Field(
-            default=None,
+            default="/cvmfs/unpacked.infn.it",
             description="Optional path for the unpacked repository in cvmfs (used to docker images lookup)"
         )
         additional_directories_in_path: List[str] = Field(
