96971f945af928d7917cfd6a3274e006b5b4e1a3
Lucio Anderlini <Lucio.Anderlini@cern.ch>
2026-02-01 15:40:34 +0100

    Changed network policy to pass through the cluster as default (attempt 4)

--- natsprovider/apptainer_cmd_builder/network.sh
+++ natsprovider/apptainer_cmd_builder/network.sh
@@ -177,9 +177,9 @@ interlink_proxy_cmd_bg /bin/bash -c "
   ip addr add %(tun_ip)s/24 dev tun0
   ip link set dev tun0 up
   # ip route add %(cluster_cidr)s via %(tun_ip)s dev tun0
-  # ip route add default via %(tun_ip)s dev tun0
   ip route del default
   ip route add default via %(tun_ip)s dev tun0
+  ip route add 10.128.0.0/16 dev tap0
   mount --bind $TMP_RESOLV_CONF /etc/resolv.conf
   exec \"%(tun2socks_binary)s\" -device tun0 -proxy \"socks5://localhost:$DPORT\" -interface tap0
 "
