235a434c287a722a72038988b8232d253fe6acc6
Lucio Anderlini <Lucio.Anderlini@cern.ch>
2026-03-13 12:50:35 +0100

    added output of init containers

--- natsprovider/apptainer_cmd_builder/ApptainerCmdBuilder.py
+++ natsprovider/apptainer_cmd_builder/ApptainerCmdBuilder.py
@@ -12,7 +12,8 @@ from .volumes import BaseVolume, FuseVolume
 from .containers import ContainerSpec
 from ..utils import generate_uid
 
-class ApptainerCmdBuilder(BaseModel, extra='forbid'):
+
+class ApptainerCmdBuilder(BaseModel, extra="forbid"):
     uid: str = Field(
         default_factory=generate_uid,
         description="Unique identifier for the set of containers",
@@ -24,17 +25,17 @@ class ApptainerCmdBuilder(BaseModel, extra='forbid'):
 
     init_containers: List[ContainerSpec] = Field(
         default=[],
-        description="List of containers representing processes to be executed simultaneously"
+        description="List of containers representing processes to be executed simultaneously",
     )
 
     additional_volumes: List[BaseVolume] = Field(
         default=[],
-        description="List of volumes to be mounted or bound to the apptainer container"
+        description="List of volumes to be mounted or bound to the apptainer container",
     )
 
     additional_directories_in_path: List[str] = Field(
         default=cfg.ADDITIONAL_DIRECTORIES_IN_PATH,
-        description="Additional directories in PATH"
+        description="Additional directories in PATH",
     )
 
     scratch_area: str = Field(
@@ -43,13 +44,12 @@ class ApptainerCmdBuilder(BaseModel, extra='forbid'):
     )
 
     description: str = Field(
-        default="",
-        description="User-defined description of the job"
+        default="", description="User-defined description of the job"
     )
 
     cachedir: str = Field(
         default=cfg.APPTAINER_CACHEDIR,
-        description="Scratch area to store apptainer images"
+        description="Scratch area to store apptainer images",
     )
 
     error_code_for_return_code_not_found: int = Field(
@@ -57,24 +57,36 @@ class ApptainerCmdBuilder(BaseModel, extra='forbid'):
         description="Numerical error code to assign in case of failure at retrieving the error code for a container.",
     )
 
-    fuse_sleep_seconds: int = Field(default=5, description="Time in seconds between fuse mount and job execution.")
+    fuse_sleep_seconds: int = Field(
+        default=5, description="Time in seconds between fuse mount and job execution."
+    )
 
     network_config: NetworkConfig = Field(
         default_factory=lambda: NetworkConfig(enabled=False),
-        description="Configuration of the network infrastructure"
+        description="Configuration of the network infrastructure",
     )
 
     @property
     def volumes(self) -> List[BaseVolume]:
         all_containers = self.containers + self.init_containers
-        return list(set([vb.volume for c in all_containers for vb in c.volume_binds] + self.additional_volumes))
+        return list(
+            set(
+                [vb.volume for c in all_containers for vb in c.volume_binds]
+                + self.additional_volumes
+            )
+        )
 
     @property
     def workdir(self):
         return os.path.join(self.scratch_area, f".acb.cluster.{self.uid}")
 
     def build_environment_files(self):
-        return '\n'.join([container.initialize() for container in self.init_containers + self.containers])
+        return "\n".join(
+            [
+                container.initialize()
+                for container in self.init_containers + self.containers
+            ]
+        )
 
     def exec_init_containers(self):
         ret = []
@@ -85,7 +97,7 @@ class ApptainerCmdBuilder(BaseModel, extra='forbid'):
                 "cp %s %s" % (container.log_path, container.log_path),
             ]
 
-        return '\n'.join(ret)
+        return "\n".join(ret)
 
     def exec_containers(self):
         ret = [
@@ -99,30 +111,46 @@ class ApptainerCmdBuilder(BaseModel, extra='forbid'):
             ]
 
         ret += [
-            "wait ${pids[%d]}; echo -n $? > %s" % (i_container, container.return_code_path)
+            "wait ${pids[%d]}; echo -n $? > %s"
+            % (i_container, container.return_code_path)
             for i_container, container in enumerate(self.containers)
         ]
 
         ret += [
             f"echo '=== Output of container {container.uid} ==='\ncat {container.log_path}"
-            for container in self.containers
+            for container in (self.init_containers or []) + (self.containers or [])
         ]
 
         ret += [
-            "tar cvf $SANDBOX/logs " + ' '.join([
-                *[f"-C {c.workdir} {os.path.basename(c.log_path)}" for c in self.init_containers],
-                *[f"-C {c.workdir} {os.path.basename(c.return_code_path)}" for c in self.init_containers],
-                *[f"-C {c.workdir} {os.path.basename(c.log_path)}" for c in self.containers],
-                *[f"-C {c.workdir} {os.path.basename(c.return_code_path)}" for c in self.containers],
-            ])
+            "tar cvf $SANDBOX/logs "
+            + " ".join(
+                [
+                    *[
+                        f"-C {c.workdir} {os.path.basename(c.log_path)}"
+                        for c in self.init_containers
+                    ],
+                    *[
+                        f"-C {c.workdir} {os.path.basename(c.return_code_path)}"
+                        for c in self.init_containers
+                    ],
+                    *[
+                        f"-C {c.workdir} {os.path.basename(c.log_path)}"
+                        for c in self.containers
+                    ],
+                    *[
+                        f"-C {c.workdir} {os.path.basename(c.return_code_path)}"
+                        for c in self.containers
+                    ],
+                ]
+            )
         ]
-        return '\n'.join(ret)
+        return "\n".join(ret)
 
     # def cleanup_environment_files(self):
     #     return '\n'.join([container.finalize() for container in self.init_containers + self.container])
 
     def build_volume_files(self):
-        ret = '\n'.join([volume.initialize() for volume in self.volumes])
+        ret = "\n".join([volume.initialize() for volume in self.volumes])
         return ret
 
     def export_of_additional_directories_in_path(self):
@@ -138,20 +166,20 @@ class ApptainerCmdBuilder(BaseModel, extra='forbid'):
 
         ret.append(f"export PATH={':'.join(links)}:$PATH")
 
-        return '\n'.join(ret)
-
-
+        return "\n".join(ret)
 
     def cleanup_volume_files(self):
         ret = []
-        if any([v.fuse_enabled_on_host and isinstance(v, FuseVolume) for v in self.volumes]):
+        if any(
+            [v.fuse_enabled_on_host and isinstance(v, FuseVolume) for v in self.volumes]
+        ):
             ret += ["sleep %d;\n" % self.fuse_sleep_seconds]
 
         # Clean-up temporary data
-        for container in (self.containers + self.init_containers):
+        for container in self.containers + self.init_containers:
             ret += [f"rm -rf {container.tmp_dir}", f"rm -rf {container.var_tmp_dir}"]
 
-        return '\n'.join(ret + [volume.finalize() for volume in self.volumes])
+        return "\n".join(ret + [volume.finalize() for volume in self.volumes])
 
     def dump(self):
         script = textwrap.dedent("""
@@ -223,7 +251,7 @@ class ApptainerCmdBuilder(BaseModel, extra='forbid'):
         
         """) % dict(
             version=version,
-            docs=''.join(["## " + line for line in self.description.splitlines()]),
+            docs="".join(["## " + line for line in self.description.splitlines()]),
             workdir=self.workdir,
             apptainer_cachedir=self.cachedir,
             environment_files=self.build_environment_files(),
@@ -238,7 +266,7 @@ class ApptainerCmdBuilder(BaseModel, extra='forbid'):
             finalize_network=self.network_config.finalize(),
         )
 
-        script_lines = script.split('\n')
+        script_lines = script.split("\n")
         script_lines = [
             next_line
             for line, next_line in zip(script_lines[:-1], script_lines[1:])
@@ -248,7 +276,7 @@ class ApptainerCmdBuilder(BaseModel, extra='forbid'):
         # if cfg.DEBUG:
         #    print ('\n'.join(script_lines))
 
-        return '\n'.join(script_lines)
+        return "\n".join(script_lines)
 
     def process_logs(self, tar_file: BinaryIO):
         """
@@ -257,17 +285,17 @@ class ApptainerCmdBuilder(BaseModel, extra='forbid'):
         """
         file_contents = {}
 
-        with tarfile.open(fileobj=tar_file, mode='r:*') as tar:
+        with tarfile.open(fileobj=tar_file, mode="r:*") as tar:
             for member in tar.getmembers():
                 if member.isfile():
-                    file_content = tar.extractfile(member).read().decode('utf-8')
+                    file_content = tar.extractfile(member).read().decode("utf-8")
                     file_contents[member.name] = file_content
 
         for container in self.containers:
             container.log = (
-                    file_contents[os.path.basename(container.log_path)]
-                    if os.path.basename(container.log_path) in file_contents.keys()
-                    else f"Error retrieving log from {container.log_path}"
+                file_contents[os.path.basename(container.log_path)]
+                if os.path.basename(container.log_path) in file_contents.keys()
+                else f"Error retrieving log from {container.log_path}"
             )
             container.return_code = int(
                 file_contents[os.path.basename(container.return_code_path)]
@@ -283,7 +311,7 @@ class ApptainerCmdBuilder(BaseModel, extra='forbid'):
             )
             init_container.return_code = int(
                 file_contents[os.path.basename(init_container.return_code_path)]
-                if (os.path.basename(init_container.return_code_path)) in file_contents.keys()
+                if (os.path.basename(init_container.return_code_path))
+                in file_contents.keys()
                 else self.error_code_for_return_code_not_found
             )
-
